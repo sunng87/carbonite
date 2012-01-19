6770cb32e677e08eeb93b034efafdd0f49c54b91
Sun Ning <classicning@gmail.com>
2012-01-19 22:03:50 +0800

    merge sritchie's branch to master, add serializers for more types

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cascalog/carbonite "1.0.5"
+(defproject info.sunng/carbonite "0.2.1"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :description "Write Clojure data to and from bytes using Kryo."
