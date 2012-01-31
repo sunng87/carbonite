6067995bb37a7f9ccf5afb3503b48a38320aba2f
Sunng <classicning@gmail.com>
2012-01-31 12:21:35 +0800

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject info.sunng/carbonite "0.2.1"
+(defproject info.sunng/carbonite "0.2.2"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :description "Write Clojure data to and from bytes using Kryo."
@@ -7,6 +7,6 @@
   :dev-dependencies [[lein-multi "1.1.0-SNAPSHOT"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
                       [com.googlecode/kryo "1.04"]]
-               "1.4" [[org.clojure/clojure "1.4.0-alpha2"]
+               "1.4" [[org.clojure/clojure "1.4.0-alpha5"]
                       [com.googlecode/kryo "1.04"]]}
   :warn-on-reflection true)
