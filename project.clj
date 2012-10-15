8998520d45c120363dfc3dc8ff8fbcf77f95df45
Sun Ning <classicning@gmail.com>
2012-10-15 21:42:41 +0800

    add tests and release 0.2.3

--- project.clj
+++ project.clj
@@ -1,12 +1,7 @@
-(defproject info.sunng/carbonite "0.2.2"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
+(defproject info.sunng/carbonite "0.2.3"
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"]
   :description "Write Clojure data to and from bytes using Kryo."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.googlecode/kryo "1.04"]]
-  :dev-dependencies [[lein-multi "1.1.0-SNAPSHOT"]]
-  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
-                      [com.googlecode/kryo "1.04"]]
-               "1.4" [[org.clojure/clojure "1.4.0-alpha5"]
-                      [com.googlecode/kryo "1.04"]]}
   :warn-on-reflection true)
--- test/carbonite/test_api.clj
+++ test/carbonite/test_api.clj
@@ -19,6 +19,12 @@
         bytes (write-bytes kryo item)]
     (read-bytes kryo bytes)))
 
+(defn round-trip-custom-buffer [item]
+  (clear-context)
+  (let [kryo (default-registry)
+        bytes (write-bytes kryo item 4096 10240)]
+    (read-bytes kryo bytes)))
+
 (defstruct mystruct :a :b)
 
 (defn new-ts [time nano]
@@ -67,6 +73,46 @@
 
 (deftest test-round-trip-compressed
   (are [obj] (is (= obj (round-trip-compressed obj)))
+       nil
+       1      ;; long
+       5.2    ;; double
+       #'+    ;; Var
+       5M     ;; BigDecimal
+       (/ 1 2) ;; Ratio
+       1000000000000000000000000  ;; BigInt
+       :foo   ;; keyword
+       :a/foo ;; namespaced keyword
+       'foo   ;; symbol
+       'a/foo ;; namespaced symbol
+       []     ;; empty vector
+       [1 2]  ;; vector
+       '()    ;; empty list
+       '(1 2) ;; list
+       #{}    ;; empty set
+       #{1 2 3}  ;; set
+       {}     ;; empty map
+       {:a 1} ;; map
+       {:a 1 :b 2} ;; map
+       {:a {:b {:c [1 #{"abc"} ]}}}  ;; nested collections
+       (Date.)  ;; java.util.Date
+       (new-ts 991000000 123456)   ;; java.sql.Timestamp
+       (java.sql.Date. 991000000)  ;; java.sql.Date
+       (java.sql.Time. 3600)       ;; java.sql.Time
+       (URI. "http://foo.com?bar=baz")  ;; java.net.URI
+       (URI. "http://\u20AC.com")  ;; java.net.URI with unicode
+       (UUID. 999 -42) 
+       (UUID. -42 999)
+       (range 50)    ;; LazySeq
+       (cons 1 '())  ;; Cons
+       (cons 1 '(2))
+       (cons 1 '(2 3))
+       (struct-map mystruct :a 1 :b 2)  ;; PersistentStructMap
+       {:a 1 :b 2 :c 3 :d 4 :e 5 :f 6 :g 7 :h 8 :i 9} ;; PersistentArrayMap
+       (seq "abc") ;; StringSeq       
+       ))
+
+(deftest test-round-trip-custom-buffer
+  (are [obj] (is (= obj (round-trip-custom-buffer obj)))
        nil
        1      ;; long
        5.2    ;; double
